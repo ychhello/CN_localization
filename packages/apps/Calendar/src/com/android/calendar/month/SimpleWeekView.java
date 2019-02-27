00690ae3abb3510ad794f1a169a1eca918eeddc8
deepwebos <deepwebos@gmail.com>
2019-02-27 15:26:31 +0800

    NO6:localization zh-CN
    Calendar

--- packages/apps/Calendar/src/com/android/calendar/month/SimpleWeekView.java
+++ packages/apps/Calendar/src/com/android/calendar/month/SimpleWeekView.java
@@ -115,6 +115,10 @@ public class SimpleWeekView extends View {
     protected Paint mMonthNumPaint;
     protected Drawable mSelectedDayLine;
 
+    protected Paint mLunarPaint;
+    protected String[] mYearNumbers;
+    protected String[] mMonthNumbers;
+
     // Cache the number strings so we don't have to recompute them each time
     protected String[] mDayNumbers;
     // Quick lookup for checking which days are in the focus month
@@ -240,6 +244,8 @@ public class SimpleWeekView extends View {
 
         // Allocate space for caching the day numbers and focus values
         mDayNumbers = new String[mNumCells];
+        mYearNumbers = new String[mNumCells];
+        mMonthNumbers = new String[mNumCells];
         mFocusDay = new boolean[mNumCells];
         mOddMonth = new boolean[mNumCells];
         mWeek = params.get(VIEW_PARAMS_WEEK);
@@ -297,6 +303,8 @@ public class SimpleWeekView extends View {
                 mHasToday = true;
                 mToday = i;
             }
+            mYearNumbers[i] = Integer.toString(time.year);
+            mMonthNumbers[i] = Integer.toString(time.month);
             mDayNumbers[i] = Integer.toString(time.monthDay++);
             time.normalize(true);
         }
